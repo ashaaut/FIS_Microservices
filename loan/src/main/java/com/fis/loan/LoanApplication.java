2337b7cc65c83975e50a8d3233a9d090ccbe8e41
ashaaut <ashaautade1998@gmail.com>
2021-08-16 15:02:39 +0530

    registered account and loan microservices on eureka

--- loan/src/main/java/com/fis/loan/LoanApplication.java
+++ loan/src/main/java/com/fis/loan/LoanApplication.java
@@ -2,8 +2,10 @@ package com.fis.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient 
 public class LoanApplication {
 
 	public static void main(String[] args) {
