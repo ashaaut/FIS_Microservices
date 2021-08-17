650a3b28a817a44ebf956123f35c7526145fe098
ashaaut <ashaautade1998@gmail.com>
2021-08-17 12:26:47 +0530

    added zuul gateway for account and loan

--- zuul-gateway/src/main/java/com/fis/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/fis/zuulgateway/ZuulGatewayApplication.java
@@ -1,10 +1,10 @@
 package com.fis.zuulgateway;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class ZuulGatewayApplication {
 
