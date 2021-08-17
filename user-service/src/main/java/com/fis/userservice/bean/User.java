7d98daf0d70cf7f33326ef7cd26fb16330c8d2f7
ashaaut <ashaautade1998@gmail.com>
2021-08-17 11:52:33 +0530

    converted monolith to microservice for user

--- user-service/src/main/java/com/fis/userservice/bean/User.java
+++ user-service/src/main/java/com/fis/userservice/bean/User.java
@@ -1,31 +1,70 @@
 package com.fis.userservice.bean;
 
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
-	int id;
-	String name;
-	public User() {
-		
-	}
-	public User(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
+
+	@Id
+	@Column(name = "us_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "us_username")
+	private String username;
+
+	@Column(name = "us_password")
+	private String password;
+
+	@ManyToMany(mappedBy = "userList", fetch = FetchType.EAGER)
+	private Set<Role> roleList;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+
+	public String getUsername() {
+		return username;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	@Override
-	public String toString() {
-		return "UserController [id=" + id + ", name=" + name + "]";
+
+	public String getPassword() {
+		return password;
 	}
-	
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoleList() {
+		return roleList;
+	}
+
+	public void setRoleList(Set<Role> roleList) {
+		this.roleList = roleList;
+	}
+
+//	@Override
+//	public String toString() {
+//		return "User [id=" + id + ", username=" + username + ", password=" + password + ", roleList=" + roleList + "]";
+//	}
+
 }
+
--- user-service/src/main/java/com/fis/userservice/controller/UserController.java
+++ user-service/src/main/java/com/fis/userservice/controller/UserController.java
@@ -1,17 +1,32 @@
 package com.fis.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fis.userservice.bean.User;
+import com.fis.userservice.repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	@GetMapping("{/id}")
-	public User getUser(@PathVariable int id) {
-		return new User(1,"asha");
+
+//	@Autowired
+//	private AppUserDetailsService appUserDetailsService;
+//
+//	@GetMapping("/{username}")
+//	public UserDetails findByUsername(@PathVariable String username) {
+//		return appUserDetailsService.loadUserByUsername(username);
+//	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/{username}")
+	public User findByUsername(@PathVariable String username) {
+		return userRepository.findByUsername(username);
 	}
+
 }
