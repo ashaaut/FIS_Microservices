2132f0f5f87eb061e926ec3860181b7c4eb9e5a9
ashaaut <ashaautade1998@gmail.com>
2021-08-17 12:51:16 +0530

    added zuul gateway for employee and user services

---
+++ zuul-gateway-eu/src/main/java/com/fis/zuulgatewayeu/ZuulGatewayEuApplication.java
@@ -0,0 +1,13 @@
+package com.fis.zuulgatewayeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayEuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayEuApplication.class, args);
+	}
+
+}
---
+++ zuul-gateway-eu/src/test/java/com/fis/zuulgatewayeu/ZuulGatewayEuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.zuulgatewayeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayEuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
